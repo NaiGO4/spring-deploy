0ffe485b252507115ae98837ad2f13884940f1b2
Giancarlos YC <giancarlosyc04@gmail.com>
2024-06-12 11:35:39 -0500

    Spring Heroku app config

---
+++ src/main/java/com/example/spring_deploy/Spring_deploy/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.spring_deploy.Spring_deploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+
+    @GetMapping("/hello")
+    public String holaMundo(){
+        System.out.println(message);
+        return "Hola Mundo con SpringBoot";
+    }
+
+    @GetMapping("/booststrap")
+    public String booststrap(){
+        return "<!doctype html>\n" +
+                "<html lang=\"en\">\n" +
+                "  <head>\n" +
+                "    <meta charset=\"utf-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "    <title>Bootstrap demo</title>\n" +
+                "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\" crossorigin=\"anonymous\">\n" +
+                "  </head>\n" +
+                "  <body>\n" +
+                "    <h1>Hello, world!</h1>\n" +
+                "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\" crossorigin=\"anonymous\"></script>\n" +
+                "  </body>\n" +
+                "</html>";
+    }
+}
